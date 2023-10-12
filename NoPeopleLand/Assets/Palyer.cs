afeb67b5c861176ebe9206caea16d3c888b3746d
JungHyon_1 <90584642+junghyonil123@users.noreply.github.com>
2023-10-12 21:20:00 +0900

    Feat: monster & Player Fight
    2023 10 12일 테스트 추후 지워주세요

--- NoPeopleLand/Assets/Palyer.cs
+++ NoPeopleLand/Assets/Palyer.cs
@@ -1,13 +1,20 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Palyer : MonoBehaviour
 {
+    public int maxHp = 100;
+    public int curHp = 100;
+    public int attactDamage = 100;
+    
+
+    public Monster enemy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -21,6 +28,10 @@ public class Palyer : MonoBehaviour
         {
             transform.position += Vector3.right;
         }
-        
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Debug.Log("АјАн");
+            enemy.GetDamage(attactDamage);
+        }
     }
 }
