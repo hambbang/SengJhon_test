1f1c164fffc26a4ab87f99c11feebb04b5c62064
simjinsung <147056288+simjinsung@users.noreply.github.com>
2023-12-14 21:03:15 +0900

    andFishing

--- NoPeopleLand/Assets/Fishing/FishingBar.cs
+++ NoPeopleLand/Assets/Fishing/FishingBar.cs
@@ -15,23 +15,32 @@ namespace Assets.Fishing
         
         void Update ()
         {
-            if (Input.GetKey(KeyCode.F))
+            if (Input.GetKeyUp(KeyCode.F))
             {
-                    if (FishingWall== Physics.BoxCast(new Vector3(transform.position.x, transform.position.y,
-                        transform.position.z),
-                        new Vector3(0.15f,0.15f,1) / 2,
-                        new Vector3(0, 0, 0)))
-                        //FishingWall= 오브젝트 boxcast(플레이어 위치 x,y,z/감지범위(0.15f,0.15f,1)/2 회전x
-                        //같을시 아래코드 실행
-                    {
+                RaycastHit2D[] hit = Physics2D.BoxCastAll(new Vector2(transform.position.x, transform.position.y), new Vector2(0.15f, 0.15f), 0f, Vector2.zero);
+                // hit RaycastHit 자료형의 변수 생성 = collider 의 정보를 받아 출력 ,함수
+                for (int i = 0; i < hit.Length; i++)
+                {
+                    RaycastHit2D ok = hit[i];
+                    //배열에 있는 정보를 단일 자료형 ok의 저장
 
-                    minigames.hookProgress = 0;
-                    minigames.failTimer = 10f;
-                    minigames.stop = false;
-                    minigames.pause = false;
-                    fishing.SetActive(true);
+                    if (ok.collider == FishingWall)//ok의 collider 형으로 변환, 비교
+                    {
+                        minigames.hookProgress = 0;
+                        minigames.failTimer = 10f;
+                        minigames.stop = false;
+                        minigames.pause = false;
+                        fishing.SetActive(true);
+
+                        fishing.gameObject.SetActive(true);
+                    }
                 }
             }
+          
+                
+
+                
+            
         }
         
         void OnTriggerStay2D(Collider2D collision)
